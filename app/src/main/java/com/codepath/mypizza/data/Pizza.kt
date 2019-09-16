2f97b0c29f4c663d2a658b328df67055e28fced4
joaopcmiranda <joaopcostamiranda@gmail.com>
2019-09-16 16:46:41 +1000

    Updated DAtaset - the changes guarantee the data will be different thus having different references, ideally the data would have an ID

--- app/src/main/java/com/codepath/mypizza/data/Pizza.kt
+++ app/src/main/java/com/codepath/mypizza/data/Pizza.kt
@@ -7,7 +7,7 @@ object Pizza {
 
     var pizzaMenu = arrayOf("ALL MEAT PIZZA", "CALIFORNIA VEGGIE", "GARLIC CHICKEN", "PANACEA MEAT", "CURRY DELIGHT", "NEW WORLD", "OLD SCHOOL", "ALL MEAT", "CALIFORNIA", "GARLIC", "MEAT", "PORK DELIGHT", "NEW HORIZON", "OLD CHAP", "RACLETE MIGHT")
 
-    var pizzaDetails = arrayOf("Premium Crushed Tomato Sauce made of 100% California grown vine ripened tomatoes topped with premium salami, pepperoni, all-natural Italian sausage and seasoned pork.",
+    var pizzaDetails = arrayOf("Premium Crushed Tomato Sauce made of 100% California grown vine ripened tomatoes topped with premium salami, pep peroni, all-natural Italian sausage and seasoned pork.",
             "Premium crushed tomato sauce, fresh green peppers, fresh red onions, fresh mushrooms, diced Roma tomatoes and fresh spinach—flavored up with our Hut Favorite on the crust edge and a balsamic sauce drizzle. Best on our Thin 'N Crispy crust.",
             "Premium crushed tomato sauce, fresh green peppers, fresh red onions, fresh mushrooms, diced Roma tomatoes and fresh spinach—flavored up with our Hut Favorite on the crust edge and a balsamic sauce drizzle. Best on our Thin 'N Crispy crust.",
             "Premium crushed tomato sauce, fresh green peppers, fresh red onions, fresh mushrooms, diced Roma tomatoes and fresh spinach—flavored up with our Hut Favorite on the crust edge and a balsamic sauce drizzle. Best on our Thin 'N Crispy crust.",
