0564110e617d0ddd23021bdcbb27afafceb0cf8c
joaopcmiranda <joaopcostamiranda@gmail.com>
2019-09-16 16:45:16 +1000

    Updated DAtaset

--- app/src/main/java/com/codepath/mypizza/data/Pizza.kt
+++ app/src/main/java/com/codepath/mypizza/data/Pizza.kt
@@ -5,7 +5,7 @@ package com.codepath.mypizza.data
  */
 object Pizza {
 
-    var pizzaMenu = arrayOf("ALL MEAT PIZZA", "CALIFORNIA VEGGIE", "GARLIC CHICKEN", "PANACEA MEAT", "CURRY DELIGHT", "NEW WORLD", "OLD SCHOOL", "ALL MEAT PIZZA", "CALIFORNIA VEGGIE", "GARLIC CHICKEN", "PANACEA MEAT", "CURRY DELIGHT", "NEW WORLD", "OLD SCHOOL", "RACLETE OVERLOAD")
+    var pizzaMenu = arrayOf("ALL MEAT PIZZA", "CALIFORNIA VEGGIE", "GARLIC CHICKEN", "PANACEA MEAT", "CURRY DELIGHT", "NEW WORLD", "OLD SCHOOL", "ALL MEAT", "CALIFORNIA", "GARLIC", "MEAT", "PORK DELIGHT", "NEW HORIZON", "OLD CHAP", "RACLETE MIGHT")
 
     var pizzaDetails = arrayOf("Premium Crushed Tomato Sauce made of 100% California grown vine ripened tomatoes topped with premium salami, pepperoni, all-natural Italian sausage and seasoned pork.",
             "Premium crushed tomato sauce, fresh green peppers, fresh red onions, fresh mushrooms, diced Roma tomatoes and fresh spinach—flavored up with our Hut Favorite on the crust edge and a balsamic sauce drizzle. Best on our Thin 'N Crispy crust.",
